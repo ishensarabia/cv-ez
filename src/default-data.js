258407b85e178863119d57c27871d386b85aa009
Ish <ishen.sarabia@gmail.com>
2025-09-20 22:16:11 -0600

    add default data

---
+++ src/default-data.js
@@ -0,0 +1,73 @@
+export const defaultData = {
+  firstName: "Ishen",
+  lastName: "Sarabia",
+  middleInitial: "",
+  jobTitle: "Web Developer",
+  summary:
+    "Dedicated  and detail-oriented Computer Science graduate with a strong foundation in full-stack web development. A consistent Dean's Lister and DOST-SEI Merit scholar, I've spent the past year mastering HTML, CSS, JavaScript, React, and Node.js through The Odin Project. Eager to leverage my skills and contribute to a dynamic team in a junior web developer role.",
+  phone: "+63 906 430 5493",
+  email: "ishen.sarabia@gmail.com",
+  website: "ishensarabia.vercel.app",
+  github: "github.com/ishensarabia",
+  location: "Mexico",
+  experiences: [
+    {
+      company: "Melonverse",
+      position: "Sr. Game Systems Engineer",
+      startDate: "July 2023",
+      endDate: "Sep 2023",
+      description:
+        "Performed basic system programming tasks and fixed system bugs gaining practical experience in troubleshooting and resolving Odoo system issues.\nGained hands-on experience with Git and GitLab workflows.\nDeveloped proficiency in Python by utilizing the language for system development and adhering to Python development standards.\nGained experience working in a Scrum environment.",
+    },
+    ,
+  ],
+  educations: [
+    {
+      school: "The Odin Project",
+      course: "Full Stack JavaScript Curriculum",
+      startDate: "Sep 2024",
+      endDate: "Current",
+      description:
+        "Key Skills Learned: HTML, CSS, Javascript, React, Node.js, Git, REST APIs, Test Driven Development, Object Oriented Programming",
+    },
+  ],
+  more: [
+    {
+      heading: "Languages",
+      list: ["HTML, CSS, Javascript, Python"],
+    },
+    {
+      heading: "Tools",
+      list: [
+        "React, Git, Github, NodeJS, Webpack, NPM, Jest, Figma, VS Code, Linux",
+      ],
+    },
+    {
+      heading: "Others",
+      list: [
+        "C, C++, Java, Visual Basic, SQLite3, Flask, PyQT5, PyCharm, IntelliJ, Firebase",
+      ],
+    },
+    {
+      heading: "Interests",
+      list: ["Front-end Development, Back-end Development, UI/UX"],
+    },
+    {
+      heading: "Certificates",
+      list: [
+        {
+          linkText: "Hardvard University's CS50",
+          url: "https://cs50.harvard.edu/certificates/b36b4087-6f03-4479-a2a2-e836306f9b4c",
+        },
+        {
+          linkText: "Cisco Network Technician",
+          url: "https://www.credly.com/badges/b073a0a7-ed8f-4e7d-81d5-426031f68a9a/public_url",
+        },
+        {
+          linkText: "Postman API Fundamentals Student Expert",
+          url: "https://api.badgr.io/public/assertions/RYaynSIaQrC2vuMBGQAdEg?identity__email=johnkellytemena%40gmail.com",
+        },
+      ],
+    },
+  ],
+};
